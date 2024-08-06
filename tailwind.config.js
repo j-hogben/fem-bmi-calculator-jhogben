c88a5acca07e1e11e02cd82be60b013326db78e6
Jot Hogben <jot_hogben@hotmail.co.uk>
2024-08-06 11:26:11 +0100

    Tailwind - Mobile styling down to and including BMI calculator

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        gunmetal: "#253347",
+        darkElectricBlue: "#5E6E85",
         blue: "#345FF6",
+        gradBottomRight: "rgba(214, 230, 254, 100%)",
+        gradTopLeft: "rgba(214, 252, 254, 0%)",
       },
       fontFamily: {
         body: "Inter",
@@ -17,9 +21,29 @@ module.exports = {
         bodyM: "1rem",
         bodyS: "0.875rem",
       },
+      letterSpacing: {
+        heading: "-0.05em",
+      },
       lineHeight: {
         110: "110%",
         150: "150%",
+        120: "120.83%",
+      },
+      height: {
+        bgPatternMob: "40rem",
+        bgPatternDesk: "46.0625rem",
+      },
+      margin: {
+        4.5: "1.125rem",
+      },
+      gap: {
+        4.5: "1.125rem",
+      },
+      borderRadius: {
+        bgPattern: "35px",
+      },
+      boxShadow: {
+        card: "1rem 2rem 3.5rem 0 rgba(143, 174, 207, 25%)",
       },
     },
   },
